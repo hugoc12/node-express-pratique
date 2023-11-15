d24e7606e7743c5510a2b35f3eb3895a075a508c
hugoc12 <hugo_c12@outlook.com>
2023-11-15 01:55:23 -0300

    ...

--- src/controller/ControllerHouses.js
+++ src/controller/ControllerHouses.js
@@ -5,16 +5,16 @@ class ControllerHouses {
     async index(request, response) {
         try {
             let idUser = request.headers.iduser;
-            if(request.query.myhouses == 'true'){
-                let listHouses = await House.find({owner:idUser});
+            if (request.query.myhouses == 'true') {
+                let listHouses = await House.find({ owner: idUser });
                 return response.json(listHouses);
-            }else{
-                let listHouses = (await House.find({status:true})).filter((el)=>{
+            } else {
+                let listHouses = (await House.find({ status: true })).filter((el) => {
                     return el.owner != idUser;
                 })//Listando casas que não pertencem ao usuário e estão disponíveis.
                 return response.json(listHouses);
             }
-            
+
         } catch (err) {
             return response.send(`ERROR - ${err}`);
         }
@@ -58,9 +58,18 @@ class ControllerHouses {
 
     async delete(request, response) {
         try {
+            let idUser = request.headers.iduser;
             let idHouse = request.params.id;
-            let houseDelete = await House.findByIdAndDelete(idHouse);
-            return response.json({houseDelete});
+            let house = await House.findById(idHouse);
+            console.log(house);
+            console.log(idUser);
+            if (house.owner == idUser) {
+                let houseDelete = await House.findByIdAndDelete(idHouse);
+                return response.json({ houseDelete });
+            } else {
+                return response.status(401).send('A casa não pertence ao usuário.')
+            }
+
         } catch (err) {
             return response.send(`ERROR: ${err}`);
         }
@@ -72,28 +81,34 @@ class ControllerHouses {
             let userId = new mongoose.Types.ObjectId(request.headers.iduser);
             let newImg = request.file;
             let houseBefore = await House.findById(request.params.id);
-            let houseUpdate = await House.findByIdAndUpdate(request.params.id, {
-                owner: userId,
-                thumbnail:newImg.filename,
-                description,
-                location:{
-                    rua,
-                    numero,
-                    bairro,
-                    cep,
-                    cidade,
-                    estado,
-                },
-                price,
-                status,
-            }, {
-                returnDocument:'after'
-            })
+            console.log(houseBefore);
+            console.log(userId);
+            if (houseBefore.owner == String(userId)) {
+                let houseUpdate = await House.findByIdAndUpdate(request.params.id, {
+                    owner: userId,
+                    thumbnail: newImg.filename,
+                    description,
+                    location: {
+                        rua,
+                        numero,
+                        bairro,
+                        cep,
+                        cidade,
+                        estado,
+                    },
+                    price,
+                    status,
+                }, {
+                    returnDocument: 'after'
+                })
 
-            return response.json({
-                houseBefore,
-                houseUpdate
-            })
+                return response.status(200).json({
+                    houseBefore,
+                    houseUpdate
+                })
+            }else{
+                return response.status(401).send('A casa não pertence ao usuário!')
+            }
         } catch (err) {
             return response.send(`ERR:${err}`);
         }
